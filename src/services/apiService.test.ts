b92e77e69cf5c5202d82e16bd22e6a4f16dea77a
Viktor <ricchiuto.viktor@gmail.com>
2021-07-14 17:40:20 +0200

    test: complete api service test

--- src/services/apiService.test.ts
+++ src/services/apiService.test.ts
@@ -1,9 +1,15 @@
-import { getAllFlights } from './apiService';
+import { getAllFlights, getAllAirports } from './apiService';
 
 describe('apiService', () => {
   describe('getAllFlights', () => {
     it('should give a list of all available flights', () => {
-      expect(getAllFlights()).toBe('mock flights');
+      return expect(getAllFlights()).resolves.toHaveLength(80);
+    });
+  });
+
+  describe('getAllAirports', () => {
+    it('should give a list of all available airports', () => {
+      return expect(getAllAirports()).resolves.toHaveLength(18);
     });
   });
 });
