29b231fb9df8fc04f870ae8235f3ead16594d114
Viktor <ricchiuto.viktor@gmail.com>
2021-07-15 20:26:06 +0200

    feat: add basic side bar component

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './SideBar.scss';
+
+function SideBar() {
+  return (
+    <div>
+      <div>✈</div>
+      <p>cerca voli</p>
+      <div>🧳</div>
+      <p>bagagli</p>
+      <div>🛌</div>
+      <p>hotel</p>
+      <div>ℹ</div>
+      <p>informazioni</p>
+      <div>banner</div>
+      <div>🤓</div>
+      <p>ciao utente</p>
+    </div>
+  );
+}
+
+export default SideBar;
