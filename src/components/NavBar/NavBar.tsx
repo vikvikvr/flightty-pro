cee89cc3a215c3eeb3b609029d167274de984232
Viktor <ricchiuto.viktor@gmail.com>
2021-07-15 20:27:21 +0200

    feat: add elements to navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 import './NavBar.scss';
 
 function NavBar() {
-  return <nav>navbar</nav>;
+  return (
+    <nav>
+      <button>🍔</button>
+      <div>♻</div>
+      <h1>FlighttyPro</h1>
+      <ul>
+        <li>Home</li>
+        <li>Logout</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
