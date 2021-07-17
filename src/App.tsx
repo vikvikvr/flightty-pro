e905ebc994526f65a52ae77bb4d9f646de7a3bd9
Viktor <ricchiuto.viktor@gmail.com>
2021-07-17 07:53:32 +0200

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import SearchPage from 'containers/SearchPage';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+
+interface RouteDescription {
+  path: string;
+  component: JSX.Element;
+}
+
+const routes: RouteDescription[] = [
+  { path: '/', component: <SearchPage /> },
+  { path: '/results', component: <div>whoops</div> },
+];
 
 function App() {
   return (
     <div className="App">
-      <SearchPage />
+      <BrowserRouter>
+        <Switch>
+          {routes.map(({ path, component }) => (
+            <Route key={path} path={path} exact children={component} />
+          ))}
+          <Redirect to="/" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
