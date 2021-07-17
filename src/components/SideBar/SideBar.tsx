6dd5812e43241ae9ca0385d28f840a5c4625b822
Viktor <ricchiuto.viktor@gmail.com>
2021-07-17 23:28:07 +0200

    refactor: improve class name

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -31,14 +31,14 @@ function SideBar(): JSX.Element {
           <p className="link-name">informazioni</p>
         </li>
       </ul>
-      <div className="line"></div>
+      <div className="horizontal-line"></div>
       <div className="bottom-part">
         <div className="banner">
           <img src={bottomBanner} alt="side banner" />
         </div>
         <li className="avatar-container">
           <UserIcon className="user-avatar" />
-          <p className="link-name ">ciao utente</p>
+          <p className="link-name ">Ciao, Utente!</p>
         </li>
       </div>
     </nav>
