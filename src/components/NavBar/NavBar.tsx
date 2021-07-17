43fc56582631cb5a767a51d66a26cd1e6cf872bf
Viktor <ricchiuto.viktor@gmail.com>
2021-07-17 09:22:41 +0200

    fix: add alt text to image

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -8,6 +8,7 @@ function NavBar() {
       <img
         className="app-logo"
         src="https://cdn.join.com/5e947ff0ac3bd8000100e449/shippy-pro-logo-l.png"
+        alt="app logo"
       />
       <h1>FlighttyPro</h1>
       <ul className="nav-links">
