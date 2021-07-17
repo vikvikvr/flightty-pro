941946519b9814ffcdf16f3d0801f1d3e53da060
Viktor <ricchiuto.viktor@gmail.com>
2021-07-17 09:27:44 +0200

    feat: make initial fetch and store result in context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import './App.scss';
 import SearchPage from 'containers/SearchPage';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import ResultsPage from 'containers/ResultsPage';
+import { AppContext } from 'types/interfaces';
+import {
+  getAllAirlines,
+  getAllAirports,
+  getAllFlights,
+} from 'services/apiService';
 
 interface RouteDescription {
   path: string;
   component: JSX.Element;
 }
+
+const defaultAppContext: AppContext = {
+  flights: [],
+  airports: [],
+  airlines: [],
+};
+
+export const appContext = createContext<AppContext>(defaultAppContext);
 
 const routes: RouteDescription[] = [
   { path: '/', component: <SearchPage /> },
@@ -15,16 +29,34 @@ const routes: RouteDescription[] = [
 ];
 
 function App() {
+  const [appState, setAppState] = useState<AppContext>(defaultAppContext);
+  useEffect(makeInitialFetch, []);
+
+  function makeInitialFetch() {
+    (async function () {
+      try {
+        const airports = await getAllAirports();
+        const airlines = await getAllAirlines();
+        const flights = await getAllFlights();
+        setAppState({ airports, airlines, flights });
+      } catch (O_o) {
+        console.log(O_o);
+      }
+    })();
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          {routes.map(({ path, component }) => (
-            <Route key={path} path={path} exact children={component} />
-          ))}
-          <Redirect to="/" />
-        </Switch>
-      </BrowserRouter>
+      <appContext.Provider value={appState}>
+        <BrowserRouter>
+          <Switch>
+            {routes.map(({ path, component }) => (
+              <Route key={path} path={path} exact children={component} />
+            ))}
+            <Redirect to="/" />
+          </Switch>
+        </BrowserRouter>
+      </appContext.Provider>
     </div>
   );
 }
