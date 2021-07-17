bd60450f77fbbe9a203f8c4aafa98c156eecafdc
Viktor <ricchiuto.viktor@gmail.com>
2021-07-17 11:49:13 +0200

    feat: add header to results page

--- src/containers/ResultsPage/ResultsPage.tsx
+++ src/containers/ResultsPage/ResultsPage.tsx
@@ -1,7 +1,7 @@
 import { appContext } from 'App';
 import FlightCard from 'components/FlightCard';
 import React, { useContext, useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { getFlightsFromTo } from 'services/apiService';
 import { populateFlights, sumFlightsCost } from 'services/flightService';
 import { Flight, SearchFlightQuery } from 'types/interfaces';
@@ -22,11 +22,19 @@ function ResultsPage() {
 
   return (
     <div>
-      <h1>search results</h1>
-      <FlightCard
-        flights={populateFlights(flights, airports, airlines)}
-        totalPrice={sumFlightsCost(flights)}
-      />
+      <div className="search-results-page">
+        <h1>Risultati ricerca</h1>
+        <div className="page-header">
+          <p className="search-query">TRO → VCE (1 scalo, € 3500)</p>
+          <Link className="search-icon" to="/">
+            🔎
+          </Link>
+        </div>
+        <FlightCard
+          flights={populateFlights(flights, airports, airlines)}
+          totalPrice={sumFlightsCost(flights)}
+        />
+      </div>
     </div>
   );
 }
