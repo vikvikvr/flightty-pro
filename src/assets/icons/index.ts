a7fcca60b9dff087f8a2bd7753f9ddb1e5f13ee8
Viktor <ricchiuto.viktor@gmail.com>
2021-07-17 15:46:09 +0200

    fix: remove inline fill from svg icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,3 +3,4 @@ export { ReactComponent as BedIcon } from './bed.svg';
 export { ReactComponent as InfoIcon } from './info.svg';
 export { ReactComponent as LuggageIcon } from './luggage.svg';
 export { ReactComponent as SearchIcon } from './search.svg';
+export { ReactComponent as UserIcon } from './user.svg';
