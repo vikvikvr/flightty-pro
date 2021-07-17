ba15f7eb1979bfe5ed880f327cb2fd35c0351fb8
Viktor <ricchiuto.viktor@gmail.com>
2021-07-17 18:48:06 +0200

    feat: add crazy planes to search page

---
+++ src/components/CrazyPlanes/CrazyPlanes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CrazyPlanes.scss';
+import { AirplaneIcon } from 'assets/icons';
+
+function CrazyPlanes() {
+  return (
+    <div className="crazy-planes-container">
+      <AirplaneIcon className="plane first" />
+      <AirplaneIcon className="plane second" />
+    </div>
+  );
+}
+
+export default CrazyPlanes;
--- src/containers/SearchPage/SearchPage.tsx
+++ src/containers/SearchPage/SearchPage.tsx
@@ -4,14 +4,22 @@ import SearchFlightForm from 'containers/SearchFlightForm';
 import React, { useContext } from 'react';
 import bottomBanner from 'assets/images/bottom-banner.jpg';
 import './SearchPage.scss';
+import CrazyPlanes from 'components/CrazyPlanes';
 
 function SearchPage() {
   const { airports } = useContext(appContext);
 
   return (
     <div className="search-page">
-      {airports.length ? <SearchFlightForm /> : <LoadingSpinner />}
-      <img className="bottom-banner" src={bottomBanner} alt="advertising" />
+      {airports.length ? (
+        <>
+          <CrazyPlanes />
+          <SearchFlightForm />
+          <img className="bottom-banner" src={bottomBanner} alt="advertising" />
+        </>
+      ) : (
+        <LoadingSpinner />
+      )}
     </div>
   );
 }
