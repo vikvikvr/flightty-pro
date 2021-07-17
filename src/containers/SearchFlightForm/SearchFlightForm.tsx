857f97f1eebf8e6156294f9c31b7c5ce80c01c56
Viktor <ricchiuto.viktor@gmail.com>
2021-07-17 12:27:21 +0200

    style: improve form layout and appearance

--- src/containers/SearchFlightForm/SearchFlightForm.tsx
+++ src/containers/SearchFlightForm/SearchFlightForm.tsx
@@ -30,53 +30,55 @@ function SearchFlightForm() {
   return (
     <div className="search-form-container">
       <form className="search-flight-form" onSubmit={handleSearchSubmit}>
-        <h1>Dove vuoi andare?</h1>
-        <label htmlFor="departureIata">Partenza:</label>
-        <select
-          id="departureIata"
-          name="departureIata"
-          value={formData.departureIata}
-          onChange={handleFormChange}
-        >
-          {airports.map((airport) => (
-            <option key={airport.id} value={airport.codeIata}>
-              {airport.codeIata}
-            </option>
-          ))}
-        </select>
-        <label htmlFor="arrivalIata">Arrivo:</label>
-        <select
-          id="arrivalIata"
-          name="arrivalIata"
-          value={formData.arrivalIata}
-          onChange={handleFormChange}
-        >
-          {airports.map((airport) => (
-            <option key={airport.id} value={airport.codeIata}>
-              {airport.codeIata}
-            </option>
-          ))}
-        </select>
-        <div className="bottom-section">
-          <div className="col">
-            <label htmlFor="maxFlights">Max scali:</label>
-            <input
-              id="maxFlights"
-              type="number"
-              value={formData.maxFlights}
-              name="maxFlights"
-              onChange={handleFormChange}
-            />
-          </div>
-          <div className="col">
-            <label htmlFor="maxPrice">Max price:</label>
-            <input
-              id="maxPrice"
-              type="number"
-              value={formData.maxPrice}
-              name="maxPrice"
-              onChange={handleFormChange}
-            />
+        <h2>Dove vuoi andare?</h2>
+        <div className="middle-section">
+          <label htmlFor="departureIata">Partenza:</label>
+          <select
+            id="departureIata"
+            name="departureIata"
+            value={formData.departureIata}
+            onChange={handleFormChange}
+          >
+            {airports.map((airport) => (
+              <option key={airport.id} value={airport.codeIata}>
+                {airport.codeIata}
+              </option>
+            ))}
+          </select>
+          <label htmlFor="arrivalIata">Arrivo:</label>
+          <select
+            id="arrivalIata"
+            name="arrivalIata"
+            value={formData.arrivalIata}
+            onChange={handleFormChange}
+          >
+            {airports.map((airport) => (
+              <option key={airport.id} value={airport.codeIata}>
+                {airport.codeIata}
+              </option>
+            ))}
+          </select>
+          <div className="bottom-section">
+            <div className="col">
+              <label htmlFor="maxFlights">Max scali:</label>
+              <input
+                id="maxFlights"
+                type="number"
+                value={formData.maxFlights}
+                name="maxFlights"
+                onChange={handleFormChange}
+              />
+            </div>
+            <div className="col">
+              <label htmlFor="maxPrice">Max price:</label>
+              <input
+                id="maxPrice"
+                type="number"
+                value={formData.maxPrice}
+                name="maxPrice"
+                onChange={handleFormChange}
+              />
+            </div>
           </div>
         </div>
         <input type="submit" name="submit" value="cerca voli" />
