cfe7e37b93fc05e6bd428ca6bc9ae4872a361280
Viktor <ricchiuto.viktor@gmail.com>
2021-07-17 07:42:03 +0200

    feat: control search flight form

--- src/containers/SearchFlightForm/SearchFlightForm.tsx
+++ src/containers/SearchFlightForm/SearchFlightForm.tsx
@@ -1,29 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Airport } from 'types/interfaces';
 import './SearchFlightForm.scss';
 
-function SearchFlightForm() {
+interface Props {
+  airports: Airport[];
+}
+
+interface State {
+  departureId: number;
+  arrivalId: number;
+  maxFlights: number;
+  maxPrice: number;
+}
+
+function SearchFlightForm({ airports }: Props) {
+  const [formData, setFormData] = useState<State>({
+    arrivalId: 1,
+    departureId: 2,
+    maxFlights: 3,
+    maxPrice: 2000,
+  });
+
+  function handleSearchSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(formData);
+  }
+
+  function handleFormChange(e: FormChangeEvent) {
+    setFormData({
+      ...formData,
+      [e.target.name]: parseFloat(e.target.value),
+    });
+  }
+
   return (
-    <form>
-      <h1>Dove vuoi andare?</h1>
-      <label htmlFor="departure">Partenza:</label>
-      <select id="departure" name="departure">
-        <option value="1">Amsterdam</option>
-        <option value="2">Buenos Aires</option>
-        <option value="3">Delhi</option>
-      </select>
-      <label htmlFor="arrival">Arrivo:</label>
-      <select id="arrival" name="arrival">
-        <option value="1">Amsterdam</option>
-        <option value="2">Buenos Aires</option>
-        <option value="3">Delhi</option>
-      </select>
-      <label htmlFor="max-flights">Max scali:</label>
-      <input id="max-flights" type="number" value={1} />
-      <label htmlFor="max-price">Max price:</label>
-      <input id="max-price" type="number" value={2_500} />
-      <input type="submit" name="submit" value="cerca voli" />
-    </form>
+    <div className="search-form-container">
+      <form className="search-flight-form" onSubmit={handleSearchSubmit}>
+        <h1>Dove vuoi andare?</h1>
+        <label htmlFor="departureId">Partenza:</label>
+        <select
+          id="departureId"
+          name="departureId"
+          value={formData.departureId}
+          onChange={handleFormChange}
+        >
+          {airports.map((airport) => (
+            <option key={airport.id} value={airport.id}>
+              {airport.codeIata}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="arrivalId">Arrivo:</label>
+        <select
+          id="arrivalId"
+          name="arrivalId"
+          value={formData.arrivalId}
+          onChange={handleFormChange}
+        >
+          {airports.map((airport) => (
+            <option key={airport.id} value={airport.id}>
+              {airport.codeIata}
+            </option>
+          ))}
+        </select>
+        <div className="bottom-section">
+          <div className="col">
+            <label htmlFor="maxFlights">Max scali:</label>
+            <input
+              id="maxFlights"
+              type="number"
+              value={formData.maxFlights}
+              name="maxFlights"
+              onChange={handleFormChange}
+            />
+          </div>
+          <div className="col">
+            <label htmlFor="maxPrice">Max price:</label>
+            <input
+              id="maxPrice"
+              type="number"
+              value={formData.maxPrice}
+              name="maxPrice"
+              onChange={handleFormChange}
+            />
+          </div>
+        </div>
+        <input type="submit" name="submit" value="cerca voli" />
+      </form>
+    </div>
   );
 }
+
+type FormChangeEvent = React.ChangeEvent<HTMLSelectElement | HTMLInputElement>;
 
 export default SearchFlightForm;
