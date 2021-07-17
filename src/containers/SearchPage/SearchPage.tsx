dc1bff164974f9efbf02a160e3428a86929d5ceb
Viktor <ricchiuto.viktor@gmail.com>
2021-07-17 17:29:54 +0200

    refactor: move banner to search page

--- src/containers/SearchPage/SearchPage.tsx
+++ src/containers/SearchPage/SearchPage.tsx
@@ -2,6 +2,7 @@ import { appContext } from 'App';
 import LoadingSpinner from 'components/LoadingSpinner';
 import SearchFlightForm from 'containers/SearchFlightForm';
 import React, { useContext } from 'react';
+import bottomBanner from 'assets/images/bottom-banner.jpg';
 import './SearchPage.scss';
 
 function SearchPage() {
@@ -10,6 +11,7 @@ function SearchPage() {
   return (
     <div className="search-page">
       {airports.length ? <SearchFlightForm /> : <LoadingSpinner />}
+      <img className="bottom-banner" src={bottomBanner} alt="advertising" />
     </div>
   );
 }
