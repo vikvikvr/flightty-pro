9ac677acdab6ecf2ad5903ebc320f79e654a062e
Viktor <ricchiuto.viktor@gmail.com>
2021-07-17 09:26:13 +0200

    feat: add ads banners and render form conditionally

--- src/containers/SearchPage/SearchPage.tsx
+++ src/containers/SearchPage/SearchPage.tsx
@@ -1,13 +1,28 @@
+import { appContext } from 'App';
 import NavBar from 'components/NavBar';
+import SideBar from 'components/SideBar';
 import SearchFlightForm from 'containers/SearchFlightForm';
-import React from 'react';
+import React, { useContext } from 'react';
 import './SearchPage.scss';
 
 function SearchPage() {
+  const { airports } = useContext(appContext);
+
   return (
-    <div>
+    <div className="search-page">
       <NavBar />
-      <SearchFlightForm />
+      {airports.length ? <SearchFlightForm /> : null}
+      <img
+        className="advertising"
+        src="https://www.exopicmedia.com/wp-content/uploads/2019/07/21-6-19__banner-08.jpg"
+        alt="advertising"
+      />
+      <SideBar />
+      <img
+        src="https://miro.medium.com/max/2000/1*31YC9s8UvFJ27bT43Ng_og.jpeg"
+        className="side-banner"
+        alt="side banner"
+      />
     </div>
   );
 }
