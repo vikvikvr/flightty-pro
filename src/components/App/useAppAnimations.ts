68a03e865864e66f2133363d81e7905e631c0bea
Viktor <ricchiuto.viktor@gmail.com>
2021-07-18 13:56:01 +0200

    feat: fade in crazy planes

--- src/components/App/useAppAnimations.ts
+++ src/components/App/useAppAnimations.ts
@@ -1,11 +1,18 @@
-import { gsap, slideLeft } from 'services/animations';
+import { fadeIn, gsap, slideLeft } from 'services/animations';
 import { useEffect } from 'react';
 
 function useAppAnimations() {
   useEffect(animateMount, []);
 
   function animateMount() {
-    gsap.timeline({ delay: 1 }).from('.right-banner', slideLeft);
+    gsap
+      .timeline({ delay: 1 })
+      .from('.right-banner', slideLeft)
+      .from('#crazy-planes', {
+        ...fadeIn,
+        delay: 1,
+        duration: 1,
+      });
   }
 }
 
