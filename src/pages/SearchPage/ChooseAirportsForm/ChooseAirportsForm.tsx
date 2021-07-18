84f0dbf4d19c364f822c5f58096851be623a1879
Viktor <ricchiuto.viktor@gmail.com>
2021-07-18 13:30:43 +0200

    feat: animate form mount

--- src/pages/SearchPage/ChooseAirportsForm/ChooseAirportsForm.tsx
+++ src/pages/SearchPage/ChooseAirportsForm/ChooseAirportsForm.tsx
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom';
 import { appContext } from 'services';
 import { AirportsQuery } from 'types';
 import './ChooseAirportsForm.scss';
+import useFormAnimations from './useFormAnimations';
 
 // **** Allows to choose departure and arrival airports ****
 
@@ -14,6 +15,7 @@ function ChooseAirportsForm(): JSX.Element {
     arrivalIata: airports[0].codeIata,
     departureIata: airports[1].codeIata,
   });
+  useFormAnimations();
 
   function handleSearchSubmit(e: React.FormEvent<HTMLFormElement>): void {
     e.preventDefault();
@@ -31,7 +33,7 @@ function ChooseAirportsForm(): JSX.Element {
   return (
     <div id="form-container">
       <form onSubmit={handleSearchSubmit}>
-        <h2>Dove vuoi andare?</h2>
+        <h2 className="title">Dove vuoi andare?</h2>
         <p className="subtitle">Nuove e fantastiche mete ti attendono...</p>
         <div className="middle-section">
           <label htmlFor="departureIata">Partenza</label>
@@ -61,12 +63,14 @@ function ChooseAirportsForm(): JSX.Element {
             ))}
           </select>
         </div>
-        <input
-          className="submit-button"
-          type="submit"
-          name="submit"
-          value="cerca voli"
-        />
+        <div className="submit-button-container">
+          <input
+            className="submit-button"
+            type="submit"
+            name="submit"
+            value="cerca voli"
+          />
+        </div>
       </form>
     </div>
   );
---
+++ src/pages/SearchPage/ChooseAirportsForm/useFormAnimations.ts
@@ -0,0 +1,17 @@
+import { gsap, slideDown, slideRight, slideUp } from 'services/animations';
+import { useEffect } from 'react';
+
+function useFormAnimations() {
+  useEffect(animateMount, []);
+
+  function animateMount() {
+    gsap
+      .timeline({ delay: 0.5 })
+      .from('#form-container', slideUp)
+      .from(['.title', '.subtitle'], { ...slideRight, stagger: 0.15 })
+      .from(['.middle-section > *'], { ...slideDown, stagger: 0.07 })
+      .from('.submit-button-container', slideUp);
+  }
+}
+
+export default useFormAnimations;
